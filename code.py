b1a24d440094dbd8c9d0f314169fd83728b2130c
MennaShazly <mnnshazly@gmail.com>
2021-10-23 13:02:20 +0200

    1st Task

--- code.py
+++ code.py
@@ -3,10 +3,23 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-#put the path to the csv file in a variable
-file = 'C:\\Users\\Menna Shazly\\Desktop\\Compiler\\data40000.csv'
-chunksize = 500
+#A class that can read a zipped csv/txt file or from a given connection string to a sqlite DB, 
+#filters, and selects named/numbered columns.
+class cls_DS_class:
 
-csv_database = create_engine('sqlite:///C:\\Users\\Menna Shazly\\Desktop\\Compiler\\database.db')
-for df in pd.read_csv(file, chunksize=chunksize, iterator=True): 
-      df.to_sql('table', csv_database, if_exists='append')
+      @staticmethod
+      def csv_sql():
+            file_path = "C:\\Users\\Menna Shazly\\Desktop\\Compiler\\data40000.zip"
+            chunksize = 1000
+
+            #create sqlLite database
+            csv_database = create_engine('sqlite:///C:\\Users\\Menna Shazly\\Desktop\\Compiler\\csv_database.db')
+
+            #read the csv file and put it into the database
+            for df in pd.read_csv(file_path, chunksize=chunksize, iterator=True): 
+                  df.to_sql('table', csv_database, if_exists='append', index=False)
+
+
+
+#test Cases
+cls_DS_class.csv_sql()
